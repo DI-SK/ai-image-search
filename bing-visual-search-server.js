98bb75ba69eb7eb8138891d5c16b0c0027178bc5
sickle <thisismedisk@gmail.com>
2025-05-04 20:06:39 +0530

    Fix fetch is not a function error for Render

--- bing-visual-search-server.js
+++ bing-visual-search-server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const fs = require('fs');
 const path = require('path');
 const FormData = require('form-data');
