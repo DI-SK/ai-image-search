b697d0e969b15c78b7e71389e87225a39be9efae
sickle <thisismedisk@gmail.com>
2025-05-07 11:23:30 +0530

    Add simple frontend interface

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ if (!fs.existsSync(dataDir)) {
 app.use(cors());
 app.use(express.json());
 
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Cache files
 const NEWS_CACHE_FILE = path.join(dataDir, 'news_cache.json');
 const VIDEOS_CACHE_FILE = path.join(dataDir, 'video_cache.json');
